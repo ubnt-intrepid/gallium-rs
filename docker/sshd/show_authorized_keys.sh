ecdd7c2303c12269326fd1e4a1a00826142480d0
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-06-19 19:02:11 +0000

    update

--- docker/sshd/show_authorized_keys.sh
+++ docker/sshd/show_authorized_keys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
 set -euo pipefail
+
 database_url="${DATABASE_URL:-postgres://postgres@db:5432/gallium}"
-exec /usr/local/bin/show_pubkeys --database-url "${database_url}"
+
+exec /opt/bin/show_pubkeys --database-url "${database_url}"
