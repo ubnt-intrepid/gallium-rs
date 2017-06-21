416168c3c3e831934f6fe488c48352fb79c43ccc
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-06-21 21:20:36 +0900

    dev

---
+++ docker/dev/docker_entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+while true
+  do sleep 10000
+done
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euo pipefail
-docker-compose run --rm dev cargo install --force --root /opt/gallium
+docker-compose exec dev cargo install --force --root /opt/gallium
