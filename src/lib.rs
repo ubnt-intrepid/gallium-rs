47b944d62b83395a6141ce45f5c752a93fc65943
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-06-22 10:32:50 +0000

    wip: API

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,16 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_codegen;
 extern crate iron;
+extern crate mount;
+extern crate bodyparser;
 extern crate router;
 extern crate urlencoded;
 extern crate flate2;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 pub mod models;
 pub mod schema;
