5cb91b8b9f1de55cf1a5c470178653b1982f75f4
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-06-22 06:42:02 +0000

    move definition of routes to src/routes.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ extern crate chrono;
 extern crate diesel;
 #[macro_use]
 extern crate diesel_codegen;
+extern crate iron;
+extern crate router;
+extern crate urlencoded;
+extern crate flate2;
 
 pub mod models;
 pub mod schema;
+pub mod routes;
