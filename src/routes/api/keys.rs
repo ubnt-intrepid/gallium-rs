759d798e37d8fa726be6b3492be28e6274faae7e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-06-22 11:29:45 +0000

    api

---
+++ src/routes/api/keys.rs
@@ -0,0 +1,67 @@
+use std::env;
+use iron::prelude::*;
+use iron::status;
+use bodyparser::Struct;
+use iron_json_response::JsonResponse;
+use diesel::insert;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use schema::public_keys;
+use models::{PublicKey, NewPublicKey, EncodablePublicKey};
+use super::ApiError;
+
+pub(super) fn handle_get_ssh_keys(_req: &mut Request) -> IronResult<Response> {
+    let conn = PgConnection::establish(&env::var("DATABASE_URL").unwrap()).unwrap();
+    let keys: Vec<EncodablePublicKey> = public_keys::table
+        .load::<PublicKey>(&conn)
+        .map_err(|err| IronError::new(err, status::InternalServerError))?
+        .into_iter()
+        .map(Into::into)
+        .collect();
+
+    Ok(Response::with((status::Ok, JsonResponse::json(&keys))))
+}
+
+pub(super) fn handle_add_ssh_key(req: &mut Request) -> IronResult<Response> {
+    #[derive(Clone, Deserialize)]
+    struct Params {
+        user_id: i32,
+        title: String,
+        key: String,
+    }
+    let params = req.get::<Struct<Params>>()
+        .ok()
+        .and_then(|s| s)
+        .ok_or_else(|| {
+            IronError::new(ApiError, (
+                status::BadRequest,
+                JsonResponse::json(&json!({
+                    "message": "Failed to parse parameter as JSON",
+                })),
+            ))
+        })?;
+
+    let new_key = NewPublicKey {
+        user_id: params.user_id,
+        title: &params.title,
+        key: &params.key,
+    };
+    let conn = PgConnection::establish(&env::var("DATABASE_URL").unwrap()).unwrap();
+    let inserted_key: EncodablePublicKey = insert(&new_key)
+        .into(public_keys::table)
+        .get_result::<PublicKey>(&conn)
+        .map(Into::into)
+        .map_err(|err| {
+            let err_message = format!("Failed to insert requested key: {}", err);
+            IronError::new(err, (
+                status::InternalServerError,
+                JsonResponse::json(&json!({
+                    "message": err_message,
+                })),
+            ))
+        })?;
+
+    Ok(Response::with(
+        (status::Created, JsonResponse::json(&inserted_key)),
+    ))
+}
---
+++ src/routes/api/mod.rs
@@ -0,0 +1,35 @@
+mod keys;
+
+use std::error;
+use std::fmt;
+use iron::prelude::*;
+use router::Router;
+use iron_json_response::JsonResponseMiddleware;
+
+
+#[derive(Debug)]
+pub struct ApiError;
+
+impl fmt::Display for ApiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "API error")
+    }
+}
+
+impl error::Error for ApiError {
+    fn description(&self) -> &str {
+        "API error"
+    }
+}
+
+
+pub fn create_api_handler() -> Chain {
+    let mut router = Router::new();
+    router.get("/user/keys", keys::handle_get_ssh_keys, "get_ssh_keys");
+    router.post("/user/keys", keys::handle_add_ssh_key, "add_ssh_key");
+
+    let mut chain = Chain::new(router);
+    chain.link_after(JsonResponseMiddleware::new());
+
+    chain
+}
