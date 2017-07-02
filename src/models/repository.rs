f044189e162f54d7740cd47dab1e97dd2937e8dd
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-07-02 20:50:13 +0900

    ls-tree

--- src/models/repository.rs
+++ src/models/repository.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::io::{self, Read};
 use std::os::unix::process::CommandExt;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use git2;
 use serde_json::Value as JsonValue;
@@ -62,31 +62,24 @@ impl Repository {
         self.inner.path()
     }
 
-    pub fn get_head_tree_objects(&self) -> AppResult<Vec<JsonValue>> {
-        let head = self.inner.head()?;
-        let target = head.target().ok_or_else(|| git2::Error::from_str(""))?;
+    pub fn list_tree(&self, refname: &str, is_recursive: bool) -> AppResult<Vec<JsonValue>> {
+        let reference = self.inner.find_reference(refname)?.resolve()?;
+        let target = reference.target().ok_or_else(|| {
+            AppError::from("failed to get target object")
+        })?;
         let commit = self.inner.find_commit(target)?;
         let tree = commit.tree()?;
-        let objects = self.collect_tree_object(&tree);
-        Ok(objects)
-    }
 
-    fn collect_tree_object(&self, tree: &git2::Tree) -> Vec<JsonValue> {
-        tree.into_iter()
-            .filter_map(|entry| {
-                let type_ = match entry.kind().unwrap() {
-                    git2::ObjectType::Blob => "blob",
-                    git2::ObjectType::Tree => "tree",
-                    _ => return None,
-                };
-                Some(json!({
-                    "id": entry.id().to_string(),
-                    "name": entry.name().unwrap(),
-                    "type": type_,
-                    "filemode": format!("{:06o}", entry.filemode()),
-                }))
-            })
-            .collect()
+        let mut objects = Vec::new();
+        walk_tree(
+            &self.inner,
+            &tree,
+            &mut objects,
+            PathBuf::default(),
+            is_recursive,
+        )?;
+
+        Ok(objects)
     }
 
     pub fn run_rpc_command<'a>(&self, service: &str, stdin: Option<&mut Box<Read + 'a>>) -> AppResult<Vec<u8>> {
@@ -112,8 +105,54 @@ impl Repository {
         if output.status.success() {
             Ok(output.stdout)
         } else {
-            let message = format!("`git {}` was exited with non-zero status: {}", service, String::from_utf8_lossy(&output.stderr));
+            let message = format!(
+                "`git {}` was exited with non-zero status: {}",
+                service,
+                String::from_utf8_lossy(&output.stderr)
+            );
             Err(io::Error::new(io::ErrorKind::Other, message).into())
         }
     }
+}
+
+
+fn walk_tree(
+    repo: &git2::Repository,
+    tree: &git2::Tree,
+    dest: &mut Vec<JsonValue>,
+    path: PathBuf,
+    is_recursive: bool,
+) -> AppResult<()> {
+    for entry in tree {
+        let id = entry.id().to_string();
+        let name = entry.name().ok_or_else(
+            || AppError::from("Failed to get entry name"),
+        )?;
+        let filemode = entry.filemode();
+        let path = path.join(&name);
+
+        let type_ = match entry.kind() {
+            Some(git2::ObjectType::Blob) => "blob",
+            Some(git2::ObjectType::Tree) => "tree",
+            _ => return Err(AppError::from("Invalid kind")),
+        };
+        if is_recursive && type_ == "tree" {
+            let tree = entry
+                .to_object(repo)
+                .ok()
+                .and_then(|o| o.into_tree().ok())
+                .unwrap();
+            walk_tree(repo, &tree, dest, path, is_recursive)?;
+        } else {
+            let item = json!({
+                "id": id,
+                "type": type_,
+                "name": &name,
+                "path": path,
+                "filemode": format!("{:06o}", filemode),
+            });
+            dest.push(item);
+        }
+    }
+    Ok(())
 }
--- src/routes/api/v1/repository.rs
+++ src/routes/api/v1/repository.rs
@@ -3,7 +3,8 @@ use iron::status;
 use iron_json_response::JsonResponse;
 use router::Router;
 use error::AppError;
-
+use url::Url;
+use std::borrow::Borrow;
 use db::DB;
 use super::error;
 use models::Project;
@@ -12,10 +13,19 @@ use models::Project;
 #[get(path = "/projects/:id/repository/tree", handler = "show_tree")]
 pub(super) struct ShowTree;
 
-// TODO: use `git ls-tree`
 fn show_tree(req: &mut Request) -> IronResult<Response> {
     let router = req.extensions.get::<Router>().unwrap();
     let id: i32 = router.find("id").and_then(|s| s.parse().ok()).unwrap();
+
+    let mut recursive: Option<bool> = None;
+    let url: Url = req.url.clone().into();
+    for (key, val) in url.query_pairs() {
+        match key.borrow() {
+            "recursive" => recursive = val.parse().ok(),
+            _ => (),
+        }
+    }
+    let recursive = recursive.unwrap_or(false);
 
     let db = req.extensions.get::<DB>().unwrap();
     let conn = db.get_db_conn().map_err(error::server_error)?;
@@ -24,7 +34,9 @@ fn show_tree(req: &mut Request) -> IronResult<Response> {
         .ok_or_else(|| IronError::new(AppError::from(""), status::NotFound))?;
     let repo = project.open_repository(&*conn).map_err(error::server_error)?;
 
-    let tree = repo.get_head_tree_objects().map_err(error::server_error)?;
+    let tree = repo.list_tree("HEAD", recursive).map_err(
+        error::server_error,
+    )?;
 
     Ok(Response::with((status::Ok, JsonResponse::json(tree))))
 }
